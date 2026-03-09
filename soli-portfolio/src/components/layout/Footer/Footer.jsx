f89585ce9a272ee24ec1ed75a565a7db565fa599
OlivaresStephanie-FS <solivares@student.fullsail.edu>
2026-03-09 01:09:16 -0400

    Making footer logo link

--- soli-portfolio/src/components/layout/Footer/Footer.jsx
+++ soli-portfolio/src/components/layout/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Container from "../../ui/Container/Container";
 import SystemStatus from "../../SystemStatus";
 import logo from "../../../assets/branding/solinyc-logo-footer.png";
@@ -9,19 +10,24 @@ function Footer() {
 	return (
 		<footer className="site-footer">
 			<Container className="site-footer__inner">
-				<div className="site-footer__brand">
-					<img
-						src={logo}
-						alt="Soli NYC logo"
-						className="site-footer__logo"
-					/>
+				<Link to="/" className="site-footer__brand">
+					<div className="site-footer__brand">
+						<img
+							src={logo}
+							alt="Soli NYC logo"
+							className="site-footer__logo"
+						/>
 
-					<div className="site-footer__brand-text">
-						<p className="site-footer__name">Stephanie Olivares</p>
-						<p className="site-footer__role">Full-Stack MERN Developer</p>
+						<div className="site-footer__brand-text">
+							<p className="site-footer__name">
+								Stephanie Olivares
+							</p>
+							<p className="site-footer__role">
+								Full-Stack MERN Developer
+							</p>
+						</div>
 					</div>
-				</div>
-
+				</Link>
 				<div className="site-footer__meta">
 					<p>© {year} soli.nyc</p>
 					<SystemStatus />
