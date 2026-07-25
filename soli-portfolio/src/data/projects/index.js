e494060a71e85f7841267450aa1f142d7fb1b875
OlivaresStephanie-FS <solivares@student.fullsail.edu>
2026-07-25 07:40:13 -0400

    Updating project list

--- soli-portfolio/src/data/projects/index.js
+++ soli-portfolio/src/data/projects/index.js
@@ -1,7 +1,7 @@
 import { contractorPlatform } from "./contractorPlatform";
 import { spotifyPrototype } from "./spotifyPrototype";
-import { memoryLaneBlog } from "./memoryLaneBlog";
 import { movievault } from "./movievault";
+import { memoryLaneBlog } from "./memoryLaneBlog";
 import { netflixClone } from "./netflixClone";
 import { severancePromo } from "./severancePromo";
 
@@ -10,8 +10,8 @@ import { severancePromo } from "./severancePromo";
 export const projects = [
 	contractorPlatform,
 	spotifyPrototype,
-	memoryLaneBlog,
 	movievault,
+	memoryLaneBlog,
 	netflixClone,
 	severancePromo,
 ];
